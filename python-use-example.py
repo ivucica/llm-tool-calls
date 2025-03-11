662c481c73ed92d409f649377439935aefb1651a
Ivan Vučica <ivan@vucica.net>
2025-03-11 21:32:46 +0000

    Enhance Wikipedia search tool description with detailed usage guidelines and error handling instructions.

--- python-use-example.py
+++ python-use-example.py
@@ -106,7 +106,14 @@ WIKI_TOOL = {
         "description": (
             "Search Wikipedia and fetch the introduction of the most relevant article. "
             "Always use this if the user is asking for something that is likely on wikipedia. "
-            "If the user has a typo in their search query, correct it before searching."
+            "If the user has a typo in their search query, correct it before searching. "
+            "For biographies, prefer searching for that individual person's article (using their full name). "
+            "For events, prefer searching for the event name. "
+            "For other topics, prefer searching for the most common name of the topic in a way that would make sense for an encyclopedia. "
+            "Don't combine multiple search queries in one call: instead of 'Nikola Tesla WW1', search for 'Nikola Tesla' and 'World War 1' separately as two tool calls. "
+            "Don't ask for 'first airing of X', instead ask just for 'X' since the API won't correctly handle the former and may return the wrong article. For example: 'first showing of The Matrix' should actually be requested with query 'The Matrix'. "
+            "If you get an error in a function call, try to fix the arguments and repeat the call. Match the arguments strictly: don't assume the tool can handle arbitrary input, missing or misformatted parameters, etc."
+            "Don't make a call using data you do not have; ask for data you need in the first round of calls, then make the call you actually want to do in the next round. For example: if you try to compute difference between 'birth of Y' and 'start of Z', first make a request for an article about 'Y' and article about 'Z'; then once you receive the response to those requests, make a request for the difference between the dates; and only then send just a response without invoking a tool."
         ),
         "parameters": {
             "type": "object",
