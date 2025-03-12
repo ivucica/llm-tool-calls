c52bf34810e85d3b3ae4fb0b891e5197ba94172e
Ivan Vučica <ivan@vucica.net>
2025-03-12 19:42:23 +0000

    Add script to run Gemini model and update .gitignore to exclude sensitive key file

---
+++ _run_gemini.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export OPENAI_API_KEY=$(cat _gemini_key.txt)
+export OPENAI_API=https://generativelanguage.googleapis.com/v1beta/openai/
+export OPENAI_MODEL=models/gemini-2.0-pro-exp-02-05
+
+#curl https://generativelanguage.googleapis.com/v1beta/openai/models \
+#  -H "Authorization: Bearer ${OPENAI_API_KEY}"
+
+
+. env/bin/activate
+
+python3 python-use-example.py
