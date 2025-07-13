a7222b30440a63e8ea7b8777804ca241b4c50a9e
Ivan Vučica <ivan@vucica.net>
2025-07-13 14:39:35 +0100

    Update _run_gemini.sh model.

--- _run_gemini.sh
+++ _run_gemini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OPENAI_API_KEY=$(cat _gemini_key.txt)
 export OPENAI_API=https://generativelanguage.googleapis.com/v1beta/openai/
-export OPENAI_MODEL=models/gemini-2.0-pro-exp-02-05
+export OPENAI_MODEL=models/gemini-2.5-pro
 
 #curl https://generativelanguage.googleapis.com/v1beta/openai/models \
 #  -H "Authorization: Bearer ${OPENAI_API_KEY}"
