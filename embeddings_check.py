9b4a9ceb029ce5e42075bb337923fb69a1d3acb7
Ivan Vučica <ivan@vucica.net>
2025-07-16 14:01:21 +0100

    embeddings_check.py: Use envvars, check schema as it is processed, etc.

--- embeddings_check.py
+++ embeddings_check.py
@@ -40,7 +40,7 @@ Authorization: Bearer 123
 {
   "model": "gaianet/text-embedding-nomic-embed-text-v1.5-embedding",
   "input": "There is a walrus in us! 1744022772"
-}   
+}
     """
     del example_request  # This is just an example request, not used in the code.
 
@@ -48,11 +48,17 @@ Authorization: Bearer 123
           f"from API endpoint: {api_endpoint} with timeout: {timeout} seconds.")
 
     conn = None
+    warnings: list[str] = []
+    resp_str: str = ""
     try:
-        host = api_endpoint.split('//')[1].split('/')[0]
+        schema = api_endpoint.split('://')[0]
+        host = api_endpoint.split('://')[1].split('/')[0]
         if not host:
             raise ValueError("Invalid API endpoint provided.")
-        conn = http.client.HTTPConnection(host, timeout=timeout)
+        conncls = http.client.HTTPConnection
+        if schema == "https":
+            conncls = http.client.HTTPSConnection
+        conn = conncls(host, timeout=timeout)
         headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
@@ -61,31 +67,74 @@ Authorization: Bearer 123
         if api_key:
             headers['Authorization'] = f'Bearer {api_key}'
 
+        path = '/' + api_endpoint.split('://')[1].split('/', 1)[1]
+
         payload = json.dumps({
             'model': model,
             'input': text
         }).encode('utf-8')
-        conn.request('POST', '/v1/embeddings', body=payload, headers=headers)
+        conn.request('POST', path, body=payload, headers=headers)
+
+        print('using host %s and path %s' % (host, path))
 
         response = conn.getresponse()
         if response.status != 200:
             raise http.client.HTTPException(f"HTTP error: {response.status} {response.reason}")
         data = response.read()
-        embeddings_str = data.decode('utf-8')
+        resp_str = data.decode('utf-8')
+
+        # response structure:
+        # { "object": "list", "data": [
+        #   { "object": "embedding", "embedding": [ 1, 2, 3 ], "index": 0 }
+        # ], "model": "modelname", "usage": { "prompt_tokens": 0, "total_tokens": 0 } }
+
         # decode json response
-        embeddings = json.loads(embeddings_str).get('embeddings', [])
-        if not isinstance(embeddings, list):
-            raise ValueError("Invalid response format: 'embeddings' should be a list.")
-        if not all(isinstance(x, float) for x in embeddings):
+        resp = json.loads(resp_str)
+        if not isinstance(resp, dict):
+            raise ValueError("Invalid response format: response should be a dict / object.")
+        if resp.get("object", None) != "list":
+            raise ValueError("Invalid response format: response should contain a list-type object, got %s instead." % resp.get("object", None))
+        if "data" not in resp.keys():
+            raise ValueError("Invalid response format: response should contain a key named 'data'.")
+        data = resp["data"]
+        if not isinstance(data, list):
+            raise ValueError("Invalid response format: 'data' should be a list / array.")
+        if len(data) == 0:
+            raise ValueError("Invalid response format: 'data' should not be zero-length.")
+        if len(data) > 1:
+            warnings.append("Got multiple objects inside data, using only the first one (index: %d)" % data[0].get("index", None))
+        data0 = data[0]
+        if data0.get("object") != "embedding":
+            raise ValueError("Invalid response format: 'data0' should contain an embedding-type object, got %s instead." % data.get("object", None))
+        if data0.get("index", None):
+            warnings.append("Got embedding object with index that is present, but not zero: %d" % data0.get("index", None))
+        embedding = data0.get("embedding", [])
+        if not isinstance(embedding, list):
+            raise ValueError("Invalid response format: 'embedding' should be a list.")
+        if len(embedding) == 0:
+            raise ValueError("Invalid response format: Empty embedding list received.")
+        if not all(isinstance(x, float) for x in embedding):
             raise ValueError(
-                "Invalid response format: all elements in 'embeddings' should be floats.")
-        if len(embeddings) == 0:
-            raise ValueError("Empty embeddings list received.")
-        return embeddings
+                "Invalid response format: all elements in 'embedding' should be floats.")
+
+        return embedding
     except http.client.HTTPException as e:
         print(f"Error fetching embeddings: {e}")
+        if not warnings:
+            # avoid printing resp_str twice
+            print(resp_str)
         return []
+    except ValueError as e:
+        if not warnings:
+            # avoid printing resp_str twice
+            print(resp_str)
+            print(resp)
+        raise
     finally:
+        if warnings:
+            print("Warnings:")
+            print(warnings)
+            print(resp_str)
         if conn:
             conn.close()
 
@@ -111,18 +160,22 @@ def main(argv: list[str] = []) -> None:
     parser.add_argument(
         '--timeout', type=float, default=5.0, help='Timeout in seconds to fetch embeddings.')
     parser.add_argument(
-        '--model', type=str, 
+        '--model', type=str,
         default=os.getenv('OPENAI_MODEL',
                           'gaianet/text-embedding-nomic-embed-text-v1.5-embedding'),
         help='Model to use for fetching embeddings.')
     parser.add_argument(
+        '--api_key', type=str,
+        default=os.getenv('OPENAI_KEY', ''),
+       help='API key to use.')
+    parser.add_argument(
         '--api_endpoint', type=str,
-        default=os.getenv('OPENAI_API', 'http://localhost:5001/v1/embeddings'),
-       help='API endpoint to fetch embeddings from.')
+        default=os.getenv('OPENAI_API', 'http://localhost:5001/v1') + '/embeddings',
+        help='API endpoint to fetch embeddings from.')
 
     args = parser.parse_args(argv)
 
-    embeddings = attempt_embeddings_fetch(args.text, args.timeout, args.model, args.api_endpoint)
+    embeddings = attempt_embeddings_fetch(args.text, args.timeout, args.model, args.api_key, args.api_endpoint)
     if embeddings:
         print(f"Fetched embeddings: {embeddings}")
     else:
