40c9044f7c0b106de1ebf5660e9fd584ee2c4c4c
Ivan Vučica <ivan@vucica.net>
2025-07-16 13:59:54 +0100

    embeddings check in Bash.

---
+++ embeddings_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OPENAI_MODEL="${OPENAI_MODEL:-gaianet/text-embedding-nomic-embed-text-v1.5-embedding}"
+OPENAI_API="${OPENAI_API:-http://localhost:5001/v1}"
+OPENAI_KEY="${OPENAI_KEY:-}"
+
+OPENAI_HEADER="${OPENAI_KEY:+-H "Authorization: Bearer ${OPENAI_KEY}"}"
+
+TEXT="${TEXT:-There is a walrus in us! $(date +%s)}"
+
+echo $OPENAI_MODEL
+echo $OPENAI_API
+if [[ -z $OPENAI_KEY ]] ; then
+  echo "no key"
+else
+  echo "has key"
+fi
+echo $TEXT
+
+set -v
+
+curl $OPENAI_HEADER -X POST -H "Content-Type: application/json" -d '{
+  "model": "gaianet/text-embedding-nomic-embed-text-v1.5-embedding",
+  "input": "${TEXT}"
+}' --max-time 15.0 "${OPENAI_API}""/embeddings"
+
